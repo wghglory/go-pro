caceae713f28db732f400a308c419b90b5884138
Guanghui Wang <guanghui-wang@foxmail.com>
2022-09-25 21:15:03 +0800

    feat: interface

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import "fmt"
 
-type Product struct {
-	name, category string
-	price          float64
+type Supplier struct {
+	name, city string
 }
 
-type Supplier struct {
+type Expense interface {
+	getName() string
+	getCost(annual bool) float64
+}
+
+type Account struct {
+	accountNumber int
+	expenses      []Expense
+}
+
+type Person struct {
 	name, city string
 }
 
@@ -43,6 +52,52 @@ func (supplier *Supplier) printDetails() {
 	fmt.Println("Supplier:", supplier.name, "City:", supplier.city)
 }
 
+func calcTotal(expenses []Expense) (total float64) {
+	for _, item := range expenses {
+		total += item.getCost(true)
+	}
+	return
+}
+
+func processItem(item interface{}) {
+	switch value := item.(type) {
+	case Product:
+		fmt.Println("Product:", value.name, "Price:", value.price)
+	case *Product:
+		fmt.Println("Product Pointer:", value.name, "Price:", value.price)
+	case Service:
+		fmt.Println("Service:", value.description, "Price:", value.monthlyFee*float64(value.durationMonths))
+	case Person:
+		fmt.Println("Person:", value.name, "City:", value.city)
+	case *Person:
+		fmt.Println("Person Pointer:", value.name, "City:", value.city)
+	case string, bool, int:
+		fmt.Println("Built-in type:", value)
+	default:
+		fmt.Println("Default:", value)
+	}
+}
+func processItems(items ...interface{}) {
+	for _, item := range items {
+		switch value := item.(type) {
+		case Product:
+			fmt.Println("Product:", value.name, "Price:", value.price)
+		case *Product:
+			fmt.Println("Product Pointer:", value.name, "Price:", value.price)
+		case Service:
+			fmt.Println("Service:", value.description, "Price:", value.monthlyFee*float64(value.durationMonths))
+		case Person:
+			fmt.Println("Person:", value.name, "City:", value.city)
+		case *Person:
+			fmt.Println("Person Pointer:", value.name, "City:", value.city)
+		case string, bool, int:
+			fmt.Println("Built-in type:", value)
+		default:
+			fmt.Println("Default:", value)
+		}
+	}
+}
+
 func main() {
 	// products := []*Product{
 	// products := ProductList{
@@ -66,4 +121,57 @@ func main() {
 	// kayak := Product{"Kayak", "Watersports", 275}  OR
 	kayak := &Product{"Kayak", "Watersports", 275}
 	kayak.printDetails()
+
+	expenses := []Expense{
+		&Product{"Kayak", "Watersports", 275},
+		Service{"Boat Cover", 12, 89.50},
+	}
+	for _, expense := range expenses {
+		fmt.Println("Expense:", expense.getName(), "Cost:", expense.getCost(true))
+	}
+	fmt.Println("Total:", calcTotal(expenses))
+
+	account := Account{accountNumber: 12345, expenses: []Expense{&Product{"Kayak", "Watersports", 275}, Service{"Boat Cover", 12, 89.50}}}
+	for _, expense := range account.expenses {
+		fmt.Println("Expense:", expense.getName(), "Cost:", expense.getCost(true))
+	}
+	fmt.Println("Total:", calcTotal(account.expenses))
+
+	expenses2 := []Expense{Service{"Boat Cover", 12, 89.50}, Service{"Paddle Protect", 12, 8}}
+	for _, expense := range expenses2 {
+		if s, ok := expense.(Service); ok {
+			fmt.Println("Service:", s.description, "Price:", s.monthlyFee*float64(s.durationMonths))
+		} else {
+			fmt.Println("Expense:", expense.getName(), "Cost:", expense.getCost(true))
+		}
+	}
+
+	for _, expense := range expenses {
+		switch value := expense.(type) {
+		case Service:
+			fmt.Println("Service:", value.description, "Price:", value.monthlyFee*float64(value.durationMonths))
+		case *Product:
+			fmt.Println("Product:", value.name, "Price:", value.price)
+		default:
+			fmt.Println("Expense:", expense.getName(), "Cost:", expense.getCost(true))
+		}
+	}
+
+	// empty interface
+	data := []interface{}{
+		expenses[0],
+		Product{"Lifejacket", "Watersports", 48.95},
+		Service{"Boat Cover", 12, 89.50},
+		Person{"Alice", "London"},
+		&Person{"Bob", "New York"},
+		"This is a string",
+		100,
+		true,
+	}
+
+	for _, item := range data {
+		processItem(item)
+	}
+
+	processItems(data...)
 }
---
+++ service.go
@@ -0,0 +1,18 @@
+package main
+
+type Service struct {
+	description    string
+	durationMonths int
+	monthlyFee     float64
+}
+
+func (s Service) getName() string {
+	return s.description
+}
+
+func (s Service) getCost(recur bool) float64 {
+	if recur {
+		return s.monthlyFee * float64(s.durationMonths)
+	}
+	return s.monthlyFee
+}
