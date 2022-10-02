498a4e1e6aac9d5f9e062badbedb02933e8be989
Guanghui Wang <guanghui-wang@foxmail.com>
2022-10-02 18:38:00 +0800

    feat: field promotion

--- composition/main.go
+++ composition/main.go
@@ -6,10 +6,21 @@ import (
 )
 
 func main() {
-	kayak := store.NewProduct("kayak", "Watersports", 275)
-	lifejacket := &store.Product{Name: "lifejacket", Category: "Watersports"}
+	// kayak := store.NewProduct("kayak", "Watersports", 275)
+	// lifejacket := &store.Product{Name: "lifejacket", Category: "Watersports"}
+	// for _, p := range []*store.Product{kayak, lifejacket} {
+	// 	fmt.Println("Name:", p.Name, "Category:", p.Category, "Price:", p.Price(0.2))
+	// }
 
-	for _, p := range []*store.Product{kayak, lifejacket} {
-		fmt.Println("Name:", p.Name, "Category:", p.Category, "Price:", p.Price(0.2))
+	boats := []*store.Boat{
+		store.NewBoat("Kayak", 275, 1, false),
+		store.NewBoat("Canoe", 400, 3, false),
+		store.NewBoat("Tender", 650.25, 2, true),
+	}
+
+	boats[0].Name = "Green Kayak"
+
+	for _, b := range boats {
+		fmt.Println("Conventional:", b.Product.Name, "Direct (field promotion):", b.Name, "Price (promotion):", b.Price(0.2))
 	}
 }
---
+++ composition/store/boat.go
@@ -0,0 +1,12 @@
+package store
+
+type Boat struct {
+	*Product  // Embedded field
+	Capacity  int
+	Motorized bool
+}
+
+func NewBoat(name string, price float64, capacity int, motorized bool) *Boat {
+	return &Boat{
+		NewProduct(name, "Watersports", price), capacity, motorized}
+}
