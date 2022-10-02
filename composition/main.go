acc8fb0a83e9d2b6e4ffba544525519f3da8fbe8
Guanghui Wang <guanghui-wang@foxmail.com>
2022-10-02 23:20:06 +0800

    feat: Using Composition to Implement Interfaces

--- composition/main.go
+++ composition/main.go
@@ -30,4 +30,12 @@ func main() {
 	for _, r := range rentals {
 		fmt.Println("Rental Boat:", r.Name, "Rental Price:", r.Price(0.2), "Captain:", r.Captain)
 	}
+
+	products := map[string]store.ItemForSale{
+		"Kayak": store.NewBoat("Kayak", 279, 1, false),
+		"Ball":  store.NewProduct("Soccer Ball", "Soccer", 19.50),
+	}
+	for key, p := range products {
+		fmt.Println("Key:", key, "Price:", p.Price(0.2))
+	}
 }
