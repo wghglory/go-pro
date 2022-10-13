e57f0182a2ca0bea657415130e343eeee1d90033
Guanghui Wang <guanghui-wang@foxmail.com>
2022-10-13 09:49:55 +0800

    feat: buffered channel

--- concurrency/operations.go
+++ concurrency/operations.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"fmt"
-	// "time"
+	"time"
 )
 
 func CalcStoreTotal(data ProductData) {
 	var storeTotal float64
-	var channel chan float64 = make(chan float64) // channel carries float64 values
+	// In real projects, a larger buffer is used, chosen so that there is sufficient capacity for goroutines to send messages without having to wait. (I usually specify a buffer size of 100, which is generally large enough for most projects but not so large that the amount of memory required is significant.)
+	var channel chan float64 = make(chan float64, 100) // channel carries float64 values, 100 is the channel buffer size
 
 	for category, group := range data {
 		// storeTotal += group.SubTotalPrice(category)
 		go group.SubTotalPrice(category, channel)
 	}
+	time.Sleep(time.Second * 2)
+
 	// one category per channel
 	for i := 0; i < len(data); i++ {
+		fmt.Println("-- channel read pending",
+			len(channel), "items in buffer, size", cap(channel))
 		// receive a result using a channel
 		// Receiving from a channel is a blocking operation, meaning that execution will not continue until a value has been received, which means I no longer have to prevent the program from terminating
 		storeTotal += <-channel
@@ -28,7 +33,7 @@ func (group ProductGroup) SubTotalPrice(category string, resultChannel chan floa
 	for _, p := range group {
 		// fmt.Println(category, "product:", p.Name)
 		total += p.Price
-		// time.Sleep(time.Millisecond * 100)
+		time.Sleep(time.Millisecond * 100)
 	}
 	fmt.Println(category, "channel sending", ToCurrency(total))
 	// send value thru resultChannel
