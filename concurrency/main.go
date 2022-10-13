ef0dbfd7eae56030b82f60b884a8f780bb55f1b7
Guanghui Wang <guanghui-wang@foxmail.com>
2022-10-13 09:23:29 +0800

    feat: use adpaters to execute functions asynchorously

--- concurrency/main.go
+++ concurrency/main.go
@@ -10,4 +10,22 @@ func main() {
 	CalcStoreTotal(Products)
 	// time.Sleep(time.Second * 5)
 	fmt.Println("main function complete")
+
+	// USING ADAPTERS TO EXECUTE FUNCTIONS ASYNCHRONOUSLY
+	calcTax := func(price float64) float64 {
+		return price + (price * 0.2)
+	}
+	resultChannel := make(chan float64)
+
+	// wrapper := func(price float64, c chan float64) {
+	// 	c <- calcTax(price)
+	// }
+	// go wrapper(275, resultChannel)
+	go func(price float64, c chan float64) {
+		c <- calcTax(price)
+	}(275, resultChannel)
+
+	result := <-resultChannel
+	fmt.Println("Result:", result)
+
 }
